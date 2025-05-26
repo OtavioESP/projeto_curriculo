6741aab7dc800c019c8e94f7ea6a9d1e9f6d4c65
Aliny <alinygomesctpm@gmail.com>
2025-05-26 20:22:39 -0300

    Adição do projeto de código

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { CurriculoComponent } from './curriculo/curriculo.component';
+
+export const routes: Routes = [
+  { path: '', component: CurriculoComponent }
+];
